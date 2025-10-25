9c80facaa4e32918da2bb29f37080621af8867ac
krsnmvk <krsnmvk@gmail.com>
2025-10-25 09:18:49 +0700

    02: database

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import { Button } from '@/components/ui/button';
+import db from '@/lib/db';
 
-export default function Home() {
+export default async function Home() {
+  const users = await db.user.findMany();
+
   return (
     <div className="min-h-screen w-screen flex flex-col gap-4 items-center justify-center">
       <div>Home Page</div>
       <Button type="button" variant="default" size="lg">
         Click Me
       </Button>
+      <div>{JSON.stringify(users, null, 2)}</div>
     </div>
   );
 }

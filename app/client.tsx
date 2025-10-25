cd2ef5b4ebdc7e00ba369328b4124d9c716a5cbc
krsnmvk <krsnmvk@gmail.com>
2025-10-25 20:10:16 +0700

    06: background jobs

--- app/client.tsx
+++ app/client.tsx
@@ -2,13 +2,26 @@
 
 import { Button } from '@/components/ui/button';
 import { authClient } from '@/lib/auth-client';
+import { useTRPC } from '@/trpc/client';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
 export default function Client() {
-  const { data, isPending } = authClient.useSession();
+  const { isPending } = authClient.useSession();
 
   const router = useRouter();
+
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+  const getWorkflows = useQuery(trpc.getWorkflows.queryOptions());
+  const createWorkflows = useMutation(
+    trpc.createWorkflows.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
 
   return (
     <div className="min-h-screen flex flex-col gap-5 items-center justify-center">
@@ -34,7 +47,17 @@ export default function Client() {
         Logout
       </Button>
 
-      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button
+        type="button"
+        onClick={() => createWorkflows.mutate()}
+        disabled={createWorkflows.isPending}
+        variant="default"
+        size="lg"
+      >
+        Create Workflows
+      </Button>
+
+      <div>{JSON.stringify(getWorkflows.data, null, 2)}</div>
     </div>
   );
 }
